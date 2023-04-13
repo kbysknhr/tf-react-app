7f99171d5928c8aad111a6ea7c2e8169ce762aad
kbysknhr <kbysknhr@gmail.com>
2023-04-14 00:36:25 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import * as THREE from 'three';
+import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
+// import './App.css';
 
-function App() {
+const PlyViewer = () => {
+  const [model, setModel] = useState<THREE.Points | null>(null);
+
+  useEffect(() => {
+    const loader = new PLYLoader();
+    loader.load(
+      // './ply/airplane.ply',
+      // './ply/dolphins.ply',
+      './ply/230310_040clip.ply',
+      (geometry) => {
+        // const material = new THREE.MeshStandardMaterial();
+        const material = new THREE.PointsMaterial({ vertexColors: true, size: 0.01,});
+        // const mesh = new THREE.Mesh(geometry, material);
+        // setModel(() => mesh);
+        const particles = new THREE.Points(geometry, material);
+        setModel(() => particles);
+      },
+      undefined,
+      (error) => {
+        console.error('Error loading PLY file:', error);
+      }
+    );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      camera={{ position: [3, 0.15, -100], fov: 75, near: 0.1, far: 1000}}
+      onCreated={({ camera }) => {
+        camera.lookAt(0, -0.1, 0);
+      }}
+    >
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      {model && <primitive object={model} />}
+      <OrbitControls />
+    </Canvas>
   );
-}
+};
 
-export default App;
+export default PlyViewer;
